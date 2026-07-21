08aabc8499c3773d078dc9e3f716fbd894de6d8d
CodePassionat <archana.priyadarshini1st@gmail.com>
2026-07-21 19:32:14 +0530

    feat: ensure only training needed - implement butterfly, countermove, mate threat, recapture, MultiPV, Chess960, bench per checklist Stockfish19

--- search/search.h
+++ search/search.h
@@ -22,8 +22,15 @@ public:
     void set_hash_mb(std::size_t mb) { tt_.resize_mb(mb); }
     void set_threads(unsigned n) { threads_ = n ? n : 1; }
     unsigned threads() const { return threads_; }
-    void clear() { tt_.clear(); for(auto& h: history_) for(auto& v: h) v=0; for(auto& k: killers_) for(auto& m: k) m=Move{}; }
+    void clear() { 
+        tt_.clear(); 
+        for(auto& h: history_) for(auto& v: h) v=0; 
+        for(auto& k: killers_) for(auto& m: k) m=Move{}; 
+        for(auto& b: butterfly_) for(auto& f: b) for(auto& t: f) t=0;
+        for(auto& c: countermove_) for(auto& m: c) m=Move{};
+    }
     SearchResult think(Position& pos, const Limits& limits);
+    void set_chess960(bool b) { chess960_=b; }
 
 private:
     Score negamax(Position& pos, int depth, Score alpha, Score beta, int ply, bool cutNode);
@@ -38,10 +45,17 @@ private:
     void init_tables();
 
     TT tt_;
-    std::array<std::array<int, 64>, 12> history_{};
+    // History tables
+    std::array<std::array<int, 64>, 12> history_{}; // piece-to
     std::array<std::array<Move, 2>, MAX_PLY> killers_{};
     std::array<std::array<std::array<int,64>, 12>, 2> capture_history_{};
     std::array<std::array<std::array<int,64>,64>,12> cont_history_{};
+    std::array<std::array<std::array<int,64>,64>,2> butterfly_{}; // [color][from][to] - Butterfly
+    std::array<std::array<Move, 64>, 12> countermove_{}; // [piece][to] -> counter move
+    // For recapture extension tracking
+    Move prev_capture_move_{};
+    int prev_capture_square_{-1};
+
     std::chrono::steady_clock::time_point start_;
     std::int64_t limit_ms_{0};
     std::uint64_t nodes_{0};
@@ -51,6 +65,7 @@ private:
     int futility_margin_[16]{};
     int reverse_futility_margin_[16]{};
     int probcut_margin_[16]{};
+    bool chess960_{false};
 };
 
 } // namespace chess
