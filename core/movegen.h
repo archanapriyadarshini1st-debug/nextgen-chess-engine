0101535d80b31ab259f7f20338042f5f8240cda8
CodePassionat <archana.priyadarshini1st@gmail.com>
2026-07-21 10:00:50 +0530

    fix: stockfish-19 roadmap - improve core/movegen.h (real SEE, TT cluster, NNUE HalfKP, Syzygy Fathom, Polyglot book, eval pawn structure, selfplay JSONL)

--- core/movegen.h
+++ core/movegen.h
@@ -1,7 +1,9 @@
-
 #pragma once
 #include "position.h"
 
 namespace chess {
 void generate_moves(Position& pos, MoveList& list, bool captures_only = false);
+int see(const Position& pos, Move m);
+bool see_ge(const Position& pos, Move m, int threshold);
+uint64_t perft(Position& pos, int depth);
 }
