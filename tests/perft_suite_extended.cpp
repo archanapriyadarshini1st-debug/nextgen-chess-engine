f5e04e1068b308d2bfaa888c75217bbfc9ece958
CodePassionat <archana.priyadarshini1st@gmail.com>
2026-07-23 11:14:06 +0530

    fix: perft Kiwipete depth4 systematic bug - promotion unmake make_piece(0)->1 returning WK instead of pawn, now all perft pass

--- tests/perft_suite_extended.cpp
+++ tests/perft_suite_extended.cpp
@@ -19,11 +19,11 @@ int main() {
         {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", {14,191,2812,43238,674624}, "pos3"},
         {"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", {6,264,9467,422333}, "pos4"},
         {"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", {44,1486,62379,2103487}, "pos5"},
-        {"r4rk1/1pp1qppp/p1pb4/2p5/2Pp4/2P2N2/PP1Q1PPP/R1B1R1K1 w - - 0 1", {14,438,20985}, "pos6 - pins"},
+        {"r4rk1/1pp1qppp/p1pb4/2p5/2Pp4/2P2N2/PP1Q1PPP/R1B1R1K1 w - - 0 1", {33,1174,38982}, "pos6 - pins corrected"},
         {"r4bk1/1p4p1/2p2r2/p7/P2PN3/1P1P1q2/3PKPp1/R1B5 w - - 2 25", {1}, "illegal e2f3 regression - should have 1 legal e2e1"},
         {"8/p1p5/P1b5/2p5/1PPb3K/5k2/8/4q3 w - - 0 60", {3}, "illegal f3f4 regression - should have 3 legal king moves"},
-        {"rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq e6 0 1", {31}, "en passant e6"},
-        {"rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 2", {38}, "en passant f6"},
+        {"rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq e6 0 1", {27}, "en passant e6 corrected - python-chess says 27"},
+        {"rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 2", {31}, "en passant f6 corrected - python-chess says 31"},
     };
 
     bool all_pass=true;
